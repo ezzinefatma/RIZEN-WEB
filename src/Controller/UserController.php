63fb4f897a39fd4da569309ef694a8375a4acf61
ezzinefatma <fatma.ezzine@esprit.tn>
2022-04-09 17:18:14 +0100

    form sign up

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +19,37 @@ class UserController extends AbstractController
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
+    }
+
+    
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(): Response
+    {
+        return $this->render('user/login.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+     /**
+     * @Route("/signup", name="signup")
+     */
+    public function signup(Request $request): Response
+    {
+
+        $user = new User();
+        $form = $this->createForm(UserType::class,$user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted())
+        {
+            $em=$this->getDoctrine()->getManager();
+            $em->persist( $user);
+            $em->flush();
+            return $this->redirectToRoute('login');
+        }
+        return $this->render("user/signup.html.twig",
+            ["f"=>$form->createView()]);
+        
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -22,6 +22,214 @@ class User
     private $idUser;
 
     /**
+     * @return int
+     */
+    public function getIdUser(): int
+    {
+        return $this->idUser;
+    }
+
+    /**
+     * @param int $idUser
+     */
+    public function setIdUser(int $idUser): void
+    {
+        $this->idUser = $idUser;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNomUser(): ?string 
+    {
+        return $this->nomUser;
+    }
+
+    /**
+     * @param string $nomUser
+     */
+    public function setNomUser(string $nomUser): void
+    {
+        $this->nomUser = $nomUser;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrenomUser(): ?string
+    {
+        return $this->prenomUser;
+    }
+
+    /**
+     * @param string $prenomUser
+     */
+    public function setPrenomUser(string $prenomUser): void
+    {
+        $this->prenomUser = $prenomUser;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getGenre(): ?string
+    {
+        return $this->genre;
+    }
+
+    /**
+     * @param string|null $genre
+     */
+    public function setGenre(?string $genre): void
+    {
+        $this->genre = $genre;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param int $age
+     */
+    public function setAge(int $age): void
+    {
+        $this->age = $age;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumeroTel():?int
+    {
+        return $this->numeroTel;
+    }
+
+    /**
+     * @param int $numeroTel
+     */
+    public function setNumeroTel(int $numeroTel): void
+    {
+        $this->numeroTel = $numeroTel;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * @param string $adresse
+     */
+    public function setAdresse(string $adresse): void
+    {
+        $this->adresse = $adresse;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string|null $username
+     */
+    public function setUsername(?string $username): void
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string|null $password
+     */
+    public function setPassword(?string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    /**
+     * @param string|null $photo
+     */
+    public function setPhoto(?string $photo): void
+    {
+        $this->photo = $photo;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatutUser()
+    {
+        return $this->statutUser;
+    }
+
+    /**
+     * @param int $statutUser
+     */
+    public function setStatutUser($statutUser): void
+    {
+        $this->statutUser = $statutUser;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param string $role
+     */
+    public function setRole(string $role): void
+    {
+        $this->role = $role;
+    }
+
+    /**
      * @var string
      *
      * @ORM\Column(name="nom_user", type="string", length=255, nullable=false)
@@ -74,8 +282,6 @@ class User
      * @var string
      *
      * @ORM\Column(name="email", type="string", length=255, nullable=false)
-     * @Assert\NotBlank(message="Email est obligatoire")
-     * @Assert\Email(message = "The email '{{ value }}' est invalide.")
      */
     private $email;
 
@@ -128,6 +334,8 @@ class User
      * )
      */
     private $idProduit;
+
+
 
     /**
      * Constructor
