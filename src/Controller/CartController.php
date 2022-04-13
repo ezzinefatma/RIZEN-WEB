72b2de2cb6a4d7b4a1d95ee054991e71c3c90be7
Aymen dhibi <dhibi.aymen@esprit.tn>
2022-04-13 20:28:20 +0100

    backand user commit

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
@@ -13,8 +16,27 @@ class CartController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
-        ]);
+        return $this->render('cart/index.html.twig'
+        );
     }
+
+    /**
+     * @Route("/carte/{idProd}", name="app_carte")
+     */
+
+     public function cart(SessionInterface  $session, Produit  $produit){
+         $panier = $session->get("panier" ,[]);
+
+        $idPro =$produit->getIdProd();
+        if(!empty($panier[$idPro])){
+            $panier[$idPro]++;
+
+        }else{
+            $panier[$idPro]=1;
+        }
+
+        $session->set("panier",$panier);
+     return $this->redirectToRoute('app_cart');
+
+     }
 }
